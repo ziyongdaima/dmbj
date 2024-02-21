ee8c83b3e094e9e11984f3399c317e7e6e3ce2e3
ziyongdaima <153406854+ziyongdaima@users.noreply.github.com>
2024-02-21 17:03:05 +0800

    Update 种子短剧.js

--- js/种子短剧.js
+++ js/种子短剧.js
@@ -7,7 +7,7 @@ var rule = {
     url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
     class_name: '短剧&KS&DY&都市&古装&重生&逆袭&虐恋&萌宝&言情&穿越&战神&神医&赘婿&甜宠&其他',
     class_url: '1&2&3&4&5&6&7&8&9&10&11&12&13&14&15&16',
-    tab_rename:{'zzdj':'公众号：肥猫宝贝'}, 
+    tab_rename:{'zzdj':'播放线路'}, 
     推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     二级: `js:
